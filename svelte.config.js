d63878a0e6c175683e99526ff0028636c487258d
Stephen Randall <stephen.randall@jhuapl.edu>
2025-01-21 17:56:06 -0500

    add pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 
+// Configuration taken from https://svelte.dev/docs/kit/adapter-static#GitHub-Pages
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
